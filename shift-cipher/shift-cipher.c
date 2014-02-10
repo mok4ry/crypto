c4d908cc1a5d8d7cd71c27c8aac62aae69b08f53
Matt Mokary <mxm6060@rit.edu>
2014-02-10 15:06:45 -0500

    Added brute forcer for shift cipher

--- shift-cipher/shift-cipher.c
+++ shift-cipher/shift-cipher.c
@@ -4,11 +4,28 @@
 #include "../lib/cipher.h"
 #include "../lib/ioutils.h"
 
+void print_most_likely(char *in) {
+    int i;
+    for (i = 1; i < 26; i++) {
+        char *shifted_alphabet = get_shifted_alphabet(i);
+        char *ciphertext = encrypt_with_alphabet(in, shifted_alphabet);
+        if (plaintext_likelihood(ciphertext) > 2) {
+            // real shift is 26 - i because we're going from ciphertext to
+            // plaintext, so working backward
+            printf("shift: %d - %s", 26 - i, ciphertext); 
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     int c, shift_number = 3; // default to Caeser cipher
+    int brute_force_break = 0;
 
-    while ((c = getopt(argc, argv, "n:")) != -1)
+    while ((c = getopt(argc, argv, "bn:")) != -1)
         switch (c) {
+            case 'b':
+                brute_force_break = 1;
+                break;
             case 'n':  // specify shift number
                 shift_number = atoi(optarg);
                 break;
@@ -18,8 +35,12 @@ int main(int argc, char *argv[]) {
                 return -1;
         }
 
-    char *shifted_alphabet = get_shifted_alphabet(shift_number);
-    printf("%s", encrypt_with_alphabet(slurp_stdin(), shifted_alphabet));
+    if (brute_force_break) {
+        print_most_likely(slurp_stdin());
+    } else {
+        char *shifted_alphabet = get_shifted_alphabet(shift_number);
+        printf("%s", encrypt_with_alphabet(slurp_stdin(), shifted_alphabet));
+    }
 
     return 0;
 }
