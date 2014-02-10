d1978bf4f30aed6b8ca36c024a46a20965db2075
Matt Mokary <mxm6060@rit.edu>
2014-02-10 15:11:19 -0500

    Added English plaintext score threshold command line option for cipher breaking

--- affine-cipher/affine-cipher.c
+++ affine-cipher/affine-cipher.c
@@ -5,7 +5,7 @@
 #include "../lib/ioutils.h"
 #include "../lib/cryptomath.h"
 
-void print_most_likely(char *in) {
+void print_most_likely(char *in, int threshold) {
     int coefficients[] = {1,3,5,7,9,11,15,17,19,21,23,25};
     int i, j;
 
@@ -13,7 +13,7 @@ void print_most_likely(char *in) {
         for (j = 1; j < 26; j++) {
             char *affine_alphabet = get_affine_alphabet(coefficients[i], j);
             char *ciphertext = encrypt_with_alphabet(in, affine_alphabet);
-            if (plaintext_likelihood(ciphertext) > 3) {
+            if (plaintext_likelihood(ciphertext) > threshold) {
                 printf("a: %d, n: %d - %s", coefficients[i], j, ciphertext);
             }
         }
@@ -22,13 +22,15 @@ void print_most_likely(char *in) {
 
 int main(int argc, char *argv[]) {
 	int c, a = 3, shift_number = 5;  // default to (3x + 5)
-    int brute_force_break = 0;
+    int brute_force_break = 0, threshold = 3;
 
-	while ((c = getopt(argc, argv, "ba:n:")) != -1)
+	while ((c = getopt(argc, argv, "bt:a:n:")) != -1)
 		switch (c) {
             case 'b':  // attempt to break input with affine cipher
                 brute_force_break = 1;
                 break;
+            case 't':
+                threshold = atoi(optarg);
 			case 'a':  // specify coefficient
 				a = atoi(optarg);
 				break;
@@ -42,7 +44,7 @@ int main(int argc, char *argv[]) {
 		}
 
 	if (brute_force_break) {
-        print_most_likely(slurp_stdin());
+        print_most_likely(slurp_stdin(), threshold);
     } else if (are_coprime(a, 26)) {
 		char *affine_alphabet = get_affine_alphabet(a, shift_number);
 		printf("%s", encrypt_with_alphabet(slurp_stdin(), affine_alphabet));
--- shift-cipher/shift-cipher.c
+++ shift-cipher/shift-cipher.c
@@ -4,12 +4,12 @@
 #include "../lib/cipher.h"
 #include "../lib/ioutils.h"
 
-void print_most_likely(char *in) {
+void print_most_likely(char *in, int threshold) {
     int i;
     for (i = 1; i < 26; i++) {
         char *shifted_alphabet = get_shifted_alphabet(i);
         char *ciphertext = encrypt_with_alphabet(in, shifted_alphabet);
-        if (plaintext_likelihood(ciphertext) > 2) {
+        if (plaintext_likelihood(ciphertext) > threshold) {
             // real shift is 26 - i because we're going from ciphertext to
             // plaintext, so working backward
             printf("shift: %d - %s", 26 - i, ciphertext); 
@@ -19,12 +19,15 @@ void print_most_likely(char *in) {
 
 int main(int argc, char *argv[]) {
     int c, shift_number = 3; // default to Caeser cipher
-    int brute_force_break = 0;
+    int brute_force_break = 0, threshold = 2;
 
-    while ((c = getopt(argc, argv, "bn:")) != -1)
+    while ((c = getopt(argc, argv, "bt:n:")) != -1)
         switch (c) {
             case 'b':
                 brute_force_break = 1;
+                break;
+            case 't':
+                threshold = atoi(optarg);
                 break;
             case 'n':  // specify shift number
                 shift_number = atoi(optarg);
@@ -36,7 +39,7 @@ int main(int argc, char *argv[]) {
         }
 
     if (brute_force_break) {
-        print_most_likely(slurp_stdin());
+        print_most_likely(slurp_stdin(), threshold);
     } else {
         char *shifted_alphabet = get_shifted_alphabet(shift_number);
         printf("%s", encrypt_with_alphabet(slurp_stdin(), shifted_alphabet));
